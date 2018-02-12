f799a68faec77f84134b340dca38255e2328ac78
maoguoping <673720904@qq.com>
2018-02-12 13:36:49 +0800

    1.api接口初始化 2.转移导航栏数据

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    if (req.query && req.query.callback) {
-        //console.log(params.query.callback);
-        res.jsonp({status: 200, message: "这是一个JSONP接口", data:[]});
-    } else {
-        res.json({status: 200, message: "这是一个JSON接口", data:[]});
+router.get('/', function (req, res) {
+    var query = req.query;
+    switch (query.method) {
+        case 'docData':
+            docData(req, res);
+            break;
+        case 'guideListData':
+            guideListData(req, res);
+            break;
     }
 });
-router.get("/docData", function (req,res) {
+function docData(req, res) {
     var docData = require('../public/data/doc/2018011901.json')
     res.json(docData)
-})
+}
+
+function guideListData(req, res) {
+    var docData = require('../public/data/guilde/guideListData.json')
+    res.json(docData)
+}
+
 module.exports = router;
