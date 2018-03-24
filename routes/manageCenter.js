ca36e8860a70166105aff2ca84c942feff9f8c03
gpmao <673720904@qq.com>
2018-03-24 16:06:46 +0800

    新增根据路径名获取列表信息的接口

--- routes/manageCenter.js
+++ routes/manageCenter.js
@@ -5,8 +5,41 @@ var jsonFile=new JsonFile();
 router.get('/getViewData', function (req, res) {
             viewData(req,res);
 });
+router.post('/getViewDataByPath', function (req, res) {
+    getViewDataByPath(req,res);
+});
 function viewData(req, res) {
     var viewData = jsonFile.read(ROOT_PATH+'/public/data/view/viewData.json');
     res.json(viewData)
+}
+function factorical(pathStr,list){
+    var listItem=list.filter(item => pathStr.indexOf(item.path)>-1);
+    if(listItem.length==0){
+        return [];
+    }
+    if(listItem[0].path!=pathStr){
+        return factorical(pathStr,listItem[0].children)
+    }else {
+        return listItem[0];
+    }
+}
+function getViewDataByPath(req, res) {
+    var viewData = jsonFile.read(ROOT_PATH+'/public/data/view/viewData.json');
+    var pathStr=decodeURI(req.body.path);
+    var resultJson={
+        data:{}
+    };
+    var result={};
+    if(pathStr==""){
+        result=viewData;
+    }else {
+        result=factorical(pathStr,viewData);
+    }
+    if(result.length==0){
+        resultJson.data=[];
+    }else {
+        resultJson.data=result;
+    }
+    res.json(resultJson);
 }
 module.exports = router;
