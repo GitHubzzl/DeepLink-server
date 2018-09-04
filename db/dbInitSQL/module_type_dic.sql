b8748fee96da1e3aed756db2ad06b81d2dc59a12
gpmao <673720904@qq.com>
2018-09-04 10:28:02 +0800

    修改初始化sql

--- db/dbInitSQL/module_type_dic.sql
+++
@@ -1,7 +0,0 @@
---模块类型列表初始化
-CREATE TABLE `module_type_dic` (
-  `id` bigint(50) NOT NULL COMMENT '主键id',
-  `module_type_id` int(50) NOT NULL COMMENT '模块类型id',
-  `module_type_name` varchar(100) NOT NULL COMMENT '模块类型名称',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
